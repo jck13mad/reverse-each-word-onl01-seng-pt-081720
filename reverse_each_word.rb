4870a3178c73379b1389cafa91afc157644b13d8
Jack Madden <jck13mad@gmail.com>
2020-09-04 22:18:03 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string)
+  
+  original_array = string.split(" ")
+  return_array = []
+  
+  original_array.each do |word|
+    return_array << word.reverse 
+  end
+  
+  return_array.join(" ")
+  
+  
+end
